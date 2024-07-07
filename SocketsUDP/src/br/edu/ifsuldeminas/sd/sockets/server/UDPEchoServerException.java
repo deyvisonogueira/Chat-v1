8aaa9e8e5c7bc9eef2493cd01371a45b18dcea28
Deyvison Nogueira <93131818+deyvisonogueira@users.noreply.github.com>
2024-07-07 18:22:58 -0300

    Delete SocketsUDP directory

--- SocketsUDP/src/br/edu/ifsuldeminas/sd/sockets/server/UDPEchoServerException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifsuldeminas.sd.sockets.server;
-
-@SuppressWarnings("serial")
-public class UDPEchoServerException extends Exception {
-	public UDPEchoServerException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
